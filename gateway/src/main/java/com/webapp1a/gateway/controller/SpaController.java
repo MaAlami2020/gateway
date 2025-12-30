ac8895b140acd9582d06687671a30163298af361
MaAlami2020 <ma.alami.2020@alumnos.urjc.es>
2025-12-30 16:37:21 +0100

    execution to production

---
+++ gateway/src/main/java/com/webapp1a/gateway/controller/SpaController.java
@@ -0,0 +1,13 @@
+package com.webapp1a.gateway.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class SpaController {
+    
+    @RequestMapping(value = "/new/**")
+    public String forward() {
+        return "forward:/new/index.html";
+    }
+}
