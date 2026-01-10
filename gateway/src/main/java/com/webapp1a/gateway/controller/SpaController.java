d27b17fa47a536eec89149f381429d4936ad179b
MaAlami2020 <ma.alami.2020@alumnos.urjc.es>
2026-01-10 21:41:48 +0100

    Update SpaController.java

--- gateway/src/main/java/com/webapp1a/gateway/controller/SpaController.java
+++ gateway/src/main/java/com/webapp1a/gateway/controller/SpaController.java
@@ -1,13 +1,22 @@
 package com.webapp1a.gateway.controller;
 
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
+import java.net.URI;
 
-@Controller
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.reactive.function.server.RequestPredicates;
+import org.springframework.web.reactive.function.server.RouterFunction;
+import org.springframework.web.reactive.function.server.RouterFunctions;
+import org.springframework.web.reactive.function.server.ServerResponse;
+
+@Configuration
 public class SpaController {
-    
-    @RequestMapping(value = "/new/**")
-    public String forward() {
-        return "redirect:/new/index.html";
+
+    @Bean
+    public RouterFunction<ServerResponse> spaRouter() {
+        return RouterFunctions.route(
+            RequestPredicates.GET("/new"),
+            req -> ServerResponse.permanentRedirect(URI.create("/new/index.html")).build()
+        );
     }
 }
