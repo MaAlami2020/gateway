0e07c3fcf5b4383583b3f5b3dc555932263f4b10
MaAlami2020 <ma.alami.2020@alumnos.urjc.es>
2026-01-10 23:03:09 +0100

    Update SpaController.java

--- gateway/src/main/java/com/webapp1a/gateway/controller/SpaController.java
+++ gateway/src/main/java/com/webapp1a/gateway/controller/SpaController.java
@@ -6,6 +6,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.io.ClassPathResource;
 import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.reactive.function.server.RequestPredicates;
 import org.springframework.web.reactive.function.server.RouterFunction;
 import org.springframework.web.reactive.function.server.RouterFunctions;
@@ -14,14 +15,8 @@ import org.springframework.web.reactive.function.server.ServerResponse;
 @Configuration
 public class SpaController {
 
-    @Bean
-    public RouterFunction<ServerResponse> spaRouter() {
-        return RouterFunctions.route(
-            RequestPredicates.GET("/new/**")
-                .and(RequestPredicates.accept(MediaType.TEXT_HTML)),
-            request -> ServerResponse.ok()
-                .contentType(MediaType.TEXT_HTML)
-                .bodyValue(new ClassPathResource("static/new/index.html"))
-        );
+     @GetMapping({"/new/**/{path:[^\\.]*}", "/new"})
+    public String forward() {
+        return "forward:/new/index.html";
     }
 }
